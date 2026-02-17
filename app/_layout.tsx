b0f8ffcce90cb7f4a7ffb6bbe35bc2a1ceb54f6f
dodowenzel <douglas.wenzel@hotmail.com>
2026-02-17 10:30:04 -0300

    fix(layout): ajustado a lógica de exibição dos componentes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,12 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
+import { ScrollProvider } from '@/src/context/ScrollContext';
+import { Navigation } from '@/src/components/Navigation';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ScrollProvider>
+      <Navigation />
+      <Stack screenOptions={{ headerShown: false }} />
+    </ScrollProvider>
+  );
 }
